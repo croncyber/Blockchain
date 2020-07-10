be508cba285afb9142b61997c8879743409ea4f6
croncyber <lyavinets@gmail.com>
2020-07-10 14:21:16 +0300

    resolve same tasks (Patterns: "Decorator", "Facade")

---
+++ Problems/Representing time as a class/src/Main.java
@@ -0,0 +1,14 @@
+class Time {
+    public int hours;
+    public int minutes;
+    public int seconds;
+
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public Time() {
+    }
+}
